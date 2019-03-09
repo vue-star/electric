f6c2a9ba401f0e94d09dc57d2bdcf671eaebec43
yanyan.wang <yanyan.wang@cloudkeeper.cn>
2019-03-09 15:34:48 +0800

    work item：SCADA的更新

--- src/view/monitor/components/yasu-scada.vue
+++ src/view/monitor/components/yasu-scada.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="couplet-card">
         <Tabs active-key="key1" type="card" style="height: 100%">
-            <Tab-pane label="1#箱变" key="key1">
+            <Tab-pane label="1#箱变" key="key3">
+                <yasu-com3 :ele-data="com3Data"></yasu-com3>
+            </Tab-pane>
+            <Tab-pane label="2#箱变" key="key1">
                 <yasu-com1 :ele-data="com1Data"></yasu-com1>
             </Tab-pane>
-            <Tab-pane label="2#箱变" key="key2">
+            <Tab-pane label="3#箱变" key="key2">
                 <yasu-com2 :ele-data="com2Data"></yasu-com2>
-            </Tab-pane>
-            <Tab-pane label="3#箱变" key="key3">
-                <yasu-com3 :ele-data="com3Data"></yasu-com3>
             </Tab-pane>
         </Tabs>
     </div>
